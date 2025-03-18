31236f38f0f5ec987a0efb6b234e7e766c67bb70
haewonwon <khcw1029@daum.net>
2025-03-18 16:47:55 +0900

    Chore: CI 진행 시, esLint 완화 정책 추가

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+};
