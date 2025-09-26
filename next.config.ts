69a14b7fa6c0cc8e2f92fd9bb1fb99adb839e71e
haewonwon <khcw1029@daum.net>
2025-09-26 11:23:22 +0900

    Chore: 테스트 파일 빌드 및 린트에서 제외 - next.config.ts에 webpack ignore-loader 추가 - eslint.config.mjs에 테스트 파일 제외 설정 추가 - middleware.test.ts 린트 에러 해결

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    // 테스트 파일들을 빌드에서 제외
+    config.module.rules.push({
+      test: /\.(test|spec)\.(ts|tsx|js|jsx)$/,
+      loader: "ignore-loader",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
